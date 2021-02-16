5c4a566c08446fb787140479a73c4e3a3bc51092
cemeris <4vitalik4@gmail.com>
2021-02-16 09:17:59 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="style.css">
+
+
+<div class="container">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+</div>
