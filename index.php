9120384158ab9808fe1e88bd433de53fc3e86c78
cemeris <4vitalik4@gmail.com>
2021-02-16 10:19:24 +0200

    added table with php loop

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <link rel="stylesheet" href="style.css">
 
-
+<?php $entries = [
+    ['', 'x', ''],
+    ['x', '', ''],
+    ['', '', 'x']
+];
+?>
 <div class="container">
-    <a href="#"></a>
-    <a href="#"></a>
-    <a href="#"></a>
-
-    <a href="#"></a>
-    <a href="#"></a>
-    <a href="#"></a>
-
-    <a href="#"></a>
-    <a href="#"></a>
-    <a href="#"></a>
+    <?php for($row = 0; $row < 3; $row++): ?>
+        <?php for($col = 0; $col < 3; $col++): ?>
+            <a href="#"><?=$row . "/" . $col; ?></a>
+        <?php endfor;?>
+    <?php endfor;?>
 </div>
+
+<pre><?=print_r($entries, true)?></pre>
+
