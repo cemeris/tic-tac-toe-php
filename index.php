bed35d95dcc2d7c2fb7455fed567538f5881dbf5
cemeris <4vitalik4@gmail.com>
2021-03-02 12:01:54 +0200

    02.03.2021. part2

--- index.php
+++ index.php
@@ -1,31 +1,42 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 ?>
 
 <link rel="stylesheet" href="style.css">
 
 <?php
 
-
 $content = file_get_contents('tictactoe_db.json');
 $entries = json_decode($content, true);
 if (!is_array($entries)) {
     $entries = [];
 }
+
 $entries['table'] = array_key_exists('table', $entries) ? $entries['table'] : [];
 
 $table = &$entries['table'];
 
 if (array_key_exists('r', $_REQUEST) && array_key_exists('c', $_REQUEST)) {
     echo "<h3>r=" . $_REQUEST['r'] . "; c= " . $_REQUEST['c'] . "</h3>";
-    $table[$_REQUEST['r']][$_REQUEST['c']] = 'x';
+
     if (!array_key_exists('count', $entries)) {
         $entries['count'] = 1;
     } else {
         $entries['count']++;
     }
+
+    $value = $entries['count'] % 2 === 0 ? 'o' : "x";
+    /*
+    if ($entries['count'] % 2 === 0) {
+        $value = 'o';
+    }
+    else {
+        $value = 'x';
+    }
+*/
+    $table[$_REQUEST['r']][$_REQUEST['c']] = $value;
 
     file_put_contents('tictactoe_db.json', json_encode($entries, JSON_PRETTY_PRINT));
 }
